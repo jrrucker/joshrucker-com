8f13385618613c595b47b71a981bd1a5ad797449
Josh Rucker <jrrucker@ncsu.edu>
2013-09-02 20:38:17 -0400

    remove flickr function

--- functions.php
+++ functions.php
@@ -135,23 +135,6 @@
 
      }
      
-     function ncsu_show_flickr_set( $set = "" , $api = "ddd4387ab0f016240787f9b72c9f9df4" ){
-         
-         ## phpFlickr Documentation: http://phpflickr.com/
-         
- 	     $f = new phpFlickr($api);
- 	     
-         $photos = $f->photosets_getPhotos($set);
-         
-         foreach ($photos['photoset']['photo'] as $photo){
-             
-             echo "<li><a rel=\"gallery\" href=\"" . $f->buildPhotoURL($photo, 'large') . "\">";
-             echo "<img src=\"" . $f->buildPhotoURL($photo, 'square') . "\" alt=\"" . $photo['title'] . "\" title=\"" . $photo['title'] . "\" width=\"75\" height=\"75\" />";
-             echo "</a></li>";
-             
-         }
-         
-     }
 
     /*****************************************************************************s
      ** v.  Short Codes
