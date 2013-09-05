a09a9056d5e8c92c4dbe9ac5d710d0c382197e74
Josh Rucker <jrrucker@ncsu.edu>
2013-09-04 23:14:08 -0400

    remove unneeded jQuery, add google analytics

--- footer.php
+++ footer.php
@@ -4,9 +4,20 @@
 
 	</div><!-- end #wrapper -->
 	
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/js/jquery.min.js"><\/script>')</script>
-	<script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
+	<script type="text/javascript">
+
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-6162050-9']);
+		_gaq.push(['_setDomainName', 'joshrucker.com']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+
+	</script>
 
 	<?php wp_footer(); ?>
 
