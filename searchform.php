ae8571cdebed7dc8e8632f19bd1cd46714859d0b
Josh Rucker <jrrucker@ncsu.edu>
2013-09-12 21:29:12 -0400

    refactor search form styles

--- searchform.php
+++ searchform.php
@@ -1,12 +1,7 @@
-
-<section id="search-box" aria-label="Search">
-	
-	<form role="search" action="<?php echo home_url( '/' ); ?>">
-		<fieldset>
-			<label for="search">Search</label>
-			<input type="search" id="search" name="s" placeholder="Search&hellip;" />
-			<input type="image" src="<?php bloginfo('template_directory'); ?>/_img/search.png" alt="Search" />
-		</fieldset>
-	</form>
-	
-</section><!-- end #search -->
+<form role="search" class="form-search" aria-label="Search" action="<?php echo home_url( '/' ); ?>">
+	<fieldset>
+		<label for="search">Search</label>
+		<input type="search" id="search" name="s" placeholder="Search&hellip;" />
+		<input type="image" src="<?php bloginfo('template_directory'); ?>/_img/search.png" alt="Search" />
+	</fieldset>
+</form>
