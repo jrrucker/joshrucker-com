f892d29cc60ce8934f8cd4f38c836dc2fea868ac
Josh Rucker <jrrucker@ncsu.edu>
2013-09-14 17:52:14 -0400

    update compass configuration (compression), move file

--- config.rb
+++ config.rb
@@ -3,18 +3,18 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "_css"
-sass_dir = "_css/"
+sass_dir = "_css"
 images_dir = "_img"
 javascripts_dir = "_js"
 fonts_dir = "_fonts"
 
-output_style = :nested
+output_style = :compressed
+environment = :production
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 color_output = false
 
 
